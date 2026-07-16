b623cd1377e92696843d6d2a90b32a652cf1cb90
alicetletech-afk <kaitlynn.ont@gmail.com>
2026-07-16 14:43:25 +0700

    Update API URL and add admin demo password

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 window.PTCAD_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwEh8xJd-XIDwHOCRRunC1CPYxYvBbrUrvxrUH5nLJlOVCCsEn2RXA3_vVIeSRUmCKn/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwF5SrWjn1RcjvkPRtBi6gc09HXA6Ty2UNDTyIfryhhfe2JpZIcwbSyfZwCwyIkZGzq/exec",
   logoUrl: "https://ptcad.alicetletech.com/assets/images/ptcad-logo.png",
   siteName: "PTCAD Sales Hub",
-  demoMode: false
+  demoMode: false,
+  adminDemoPassword: "ptcad2026"
 };
